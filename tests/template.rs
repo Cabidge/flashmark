6f3069b906284c9122f325afa5416708d50b27b2
Cabidge <cabbageboi69@gmail.com>
2023-09-14 14:09:40 -0400

    update test cases to have separate test_render function

--- tests/template.rs
+++ tests/template.rs
@@ -1,4 +1,9 @@
-fn test_render_slide(scope: &mut rhai::Scope<'static>, input: &str, expected: &str) {
+fn test_render(input: &str, expected: &str) {
+    let actual = flashmark::template::render(input);
+    assert_eq!(actual, expected);
+}
+
+fn test_render_with_scope(scope: &mut rhai::Scope<'static>, input: &str, expected: &str) {
     use flashmark::template;
     let actual = template::render_with_engine_and_scope(&template::new_engine(), scope, input);
 
@@ -6,17 +11,23 @@ fn test_render_slide(scope: &mut rhai::Scope<'static>, input: &str, expected: &s
 }
 
 #[test]
+fn literal() {
+    test_render("Hello, World!", "Hello, World!");
+}
+
+#[test]
+fn literal_with_at() {
+    test_render("Hello, @World!", "Hello, @World!");
+}
+
+#[test]
 fn expression_string() {
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "Hello, @(\"World\")!",
-        "Hello, World!",
-    );
+    test_render("Hello, @(\"World\")!", "Hello, World!");
 }
 
 #[test]
 fn expression_arithmetic() {
-    test_render_slide(&mut rhai::Scope::new(), "1 + 2 = @(1 + 2)", "1 + 2 = 3");
+    test_render("1 + 2 = @(1 + 2)", "1 + 2 = 3");
 }
 
 #[test]
@@ -24,7 +35,7 @@ fn expression_variables() {
     let mut scope = rhai::Scope::new();
     scope.push("name", "World");
 
-    test_render_slide(&mut scope, "Hello, @(name)!", "Hello, World!");
+    test_render_with_scope(&mut scope, "Hello, @(name)!", "Hello, World!");
 }
 
 #[test]
@@ -33,65 +44,33 @@ fn expression_variables_and_arithmetic() {
     scope.push("x", 1);
     scope.push("y", 2);
 
-    test_render_slide(&mut scope, "@(x) + @(y) = @(x + y)", "1 + 2 = 3");
+    test_render_with_scope(&mut scope, "@(x) + @(y) = @(x + y)", "1 + 2 = 3");
 }
 
 #[test]
 fn if_true_and_false() {
-    test_render_slide(&mut rhai::Scope::new(), "@if true { true }", " true ");
-    test_render_slide(&mut rhai::Scope::new(), "@if false { true }", "");
+    test_render("@if true { true }", " true ");
+    test_render("@if false { true }", "");
 }
 
 #[test]
 fn if_else() {
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if true { true } @else { false }",
-        " true ",
-    );
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if false { true } @else { false }",
-        " false ",
-    );
+    test_render("@if true { true } @else { false }", " true ");
+    test_render("@if false { true } @else { false }", " false ");
 }
 
 #[test]
 fn if_elif() {
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if true { 1 } @elif true { 2 }",
-        " 1 ",
-    );
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if false { 1 } @elif true { 2 }",
-        " 2 ",
-    );
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if false { 1 } @elif false { 2 }",
-        "",
-    );
+    test_render("@if true { 1 } @elif true { 2 }", " 1 ");
+    test_render("@if false { 1 } @elif true { 2 }", " 2 ");
+    test_render("@if false { 1 } @elif false { 2 }", "");
 }
 
 #[test]
 fn if_elif_else() {
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if true { 1 } @elif true { 2 } @else { 3 }",
-        " 1 ",
-    );
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if false { 1 } @elif true { 2 } @else { 3 }",
-        " 2 ",
-    );
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if false { 1 } @elif false { 2 } @else { 3 }",
-        " 3 ",
-    );
+    test_render("@if true { 1 } @elif true { 2 } @else { 3 }", " 1 ");
+    test_render("@if false { 1 } @elif true { 2 } @else { 3 }", " 2 ");
+    test_render("@if false { 1 } @elif false { 2 } @else { 3 }", " 3 ");
 }
 
 #[test]
@@ -99,17 +78,13 @@ fn if_expression() {
     let mut scope = rhai::Scope::new();
     scope.push("x", 1_i64);
 
-    test_render_slide(&mut scope, "@if x == 1 { 1 } @else { 2 }", " 1 ");
-    test_render_slide(&mut scope, "@if x == 2 { 1 } @else { 2 }", " 2 ");
+    test_render_with_scope(&mut scope, "@if x == 1 { 1 } @else { 2 }", " 1 ");
+    test_render_with_scope(&mut scope, "@if x == 2 { 1 } @else { 2 }", " 2 ");
 }
 
 #[test]
 fn if_newline() {
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@if true {\n    true\n}",
-        "    true",
-    );
+    test_render("@if true {\n    true\n}", "    true");
 }
 
 #[test]
@@ -117,7 +92,7 @@ fn nested() {
     let mut scope = rhai::Scope::new();
     scope.push("name", "World");
 
-    test_render_slide(
+    test_render_with_scope(
         &mut scope,
         "Hello, @(name)!\n@if true { Hello, @(name)! }",
         "Hello, World!\n Hello, World! ",
@@ -126,11 +101,7 @@ fn nested() {
 
 #[test]
 fn for_array() {
-    test_render_slide(
-        &mut rhai::Scope::new(),
-        "@for x in [1, 2, 3] { @(x) }",
-        " 1  2  3 ",
-    );
+    test_render("@for x in [1, 2, 3] { @(x) }", " 1  2  3 ");
 }
 
 #[test]
@@ -139,7 +110,7 @@ fn for_expression() {
     let arr: rhai::Array = vec![1.into(), 2.into(), 3.into()];
     scope.push("arr", arr);
 
-    test_render_slide(&mut scope, "@for x in arr { @(x) }", " 1  2  3 ");
+    test_render_with_scope(&mut scope, "@for x in arr { @(x) }", " 1  2  3 ");
 }
 
 #[test]
@@ -148,7 +119,7 @@ fn for_nested() {
     let arr: rhai::Array = vec![1.into(), 2.into(), 3.into()];
     scope.push("arr", arr);
 
-    test_render_slide(
+    test_render_with_scope(
         &mut scope,
         "@for x in arr {@for y in arr { (@(x), @(y)) }}",
         " (1, 1)  (1, 2)  (1, 3)  (2, 1)  (2, 2)  (2, 3)  (3, 1)  (3, 2)  (3, 3) ",
