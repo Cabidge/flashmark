ecee37af7f9fc767431427776f01e6b76810fe27
Cabidge <cabbageboi69@gmail.com>
2023-09-21 14:04:11 -0400

    add a try_tokenize_keyword function

--- src/math/parse/tokenize/mod.rs
+++ src/math/parse/tokenize/mod.rs
@@ -12,6 +12,103 @@ impl<'a> Tokenizer<'a> {
             parser: StrParser::new(input),
         }
     }
+
+    fn try_tokenize_keyword(&mut self) -> Option<token::Keyword> {
+        use token::{Function, Grouping, GroupingKind, GroupingSide, Keyword, Symbol};
+
+        let mut symbol_mapping = vec![
+            ("+", Symbol::Plus),
+            ("-", Symbol::Minus),
+            ("/", Symbol::Slash),
+            ("*", Symbol::DotProduct),
+            ("xx", Symbol::CrossProduct),
+            ("^", Symbol::Caret),
+            ("_", Symbol::Underscore),
+            ("=", Symbol::Equal),
+            ("!=", Symbol::NotEqual),
+            ("<", Symbol::LessThan),
+            (">", Symbol::GreaterThan),
+            ("<=", Symbol::LessThanOrEqual),
+            (">=", Symbol::GreaterThanOrEqual),
+            (":", Symbol::Colon),
+            ("in", Symbol::In),
+            ("notin", Symbol::NotIn),
+            ("sum", Symbol::Sum),
+            ("int", Symbol::Integral),
+        ];
+
+        let grouping_mapping = [
+            ("(", ")", GroupingKind::Paren),
+            ("[", "]", GroupingKind::Bracket),
+            ("{", "}", GroupingKind::Brace),
+        ];
+
+        // add grouping symbols to the symbol mapping
+        symbol_mapping.extend(
+            grouping_mapping
+                .into_iter()
+                .flat_map(|(left, right, kind)| {
+                    [
+                        (left, kind, GroupingSide::Left),
+                        (right, kind, GroupingSide::Right),
+                    ]
+                })
+                .map(|(symbol, kind, side)| (symbol, Symbol::Grouping(Grouping { kind, side }))),
+        );
+
+        let function_mapping = [
+            ("sqrt", Function::Sqrt),
+            ("sin", Function::Sin),
+            ("cos", Function::Cos),
+            ("tan", Function::Tan),
+        ];
+
+        let mut keyword_mapping = vec![];
+
+        // add symbol mappings to the keyword mappings
+        keyword_mapping.extend(
+            symbol_mapping
+                .into_iter()
+                .map(|(symbol, keyword)| (symbol, Keyword::Symbol(keyword))),
+        );
+
+        // add function mappings to the keyword mappings
+        keyword_mapping.extend(
+            function_mapping
+                .into_iter()
+                .map(|(symbol, keyword)| (symbol, Keyword::Function(keyword))),
+        );
+
+        let mut parser = self.parser.clone();
+
+        // remember the latest keyword match and the parser state
+        let mut last_match: Option<(Keyword, StrParser<'a>)> = None;
+
+        // loop until we have no more keywords to match
+        while !keyword_mapping.is_empty() {
+            let ch = parser.advance()?;
+
+            // remove all keywords that don't match the current character
+            keyword_mapping.retain_mut(|(symbol, _)| {
+                let Some(rest) = symbol.strip_prefix(ch) else { return false; };
+                *symbol = rest;
+                true
+            });
+
+            if let Some(keyword) = keyword_mapping
+                .iter()
+                .find_map(|(symbol, keyword)| symbol.is_empty().then_some(*keyword))
+            {
+                // if we found a keyword, we want to remember the parser state
+                last_match = Some((keyword, parser.clone()));
+            }
+        }
+
+        last_match.map(|(keyword, parser)| {
+            self.parser = parser;
+            keyword
+        })
+    }
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
