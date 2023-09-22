60e017fcb7cccb10ae0f007b4db6b040c6470bd1
Cabidge <cabbageboi69@gmail.com>
2023-09-22 17:38:47 -0400

    add Parser struct

--- src/math/parse/mod.rs
+++ src/math/parse/mod.rs
@@ -1,1 +1,13 @@
 pub mod tokenize;
+
+pub struct Parser<'a> {
+    token_stream: tokenize::Tokenizer<'a>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            token_stream: tokenize::Tokenizer::new(input),
+        }
+    }
+}
