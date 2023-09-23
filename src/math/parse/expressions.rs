1fb6acbbf15c80d19c075a9228c7ce5fa631ffae
Cabidge <cabbageboi69@gmail.com>
2023-09-23 18:49:59 -0400

    derive Debug and Clone

--- src/math/parse/expressions.rs
+++ src/math/parse/expressions.rs
@@ -1,21 +1,25 @@
 use super::tokenize::token::{Function, GroupingKind};
 
+#[derive(Debug, Clone)]
 pub enum Expr {
     Unit(Box<UnitExpr>),
     Fraction(Box<Fraction>),
 }
 
+#[derive(Debug, Clone)]
 pub struct UnitExpr {
     pub variant: ExprVariant,
     pub super_script: Option<Expr>,
     pub sub_script: Option<Expr>,
 }
 
+#[derive(Debug, Clone)]
 pub struct Fraction {
     pub numerator: Expr,
     pub denominator: Expr,
 }
 
+#[derive(Debug, Clone)]
 pub enum ExprVariant {
     Identifier(Box<str>),
     Operator(Box<str>),
