53e706eeef90e562c5b920d6f93f7515a6bfdc41
Cabidge <cabbageboi69@gmail.com>
2023-09-23 18:49:04 -0400

    extract structs from enum variants

--- src/math/parse/expressions.rs
+++ src/math/parse/expressions.rs
@@ -1,15 +1,19 @@
 use super::tokenize::token::{Function, GroupingKind};
 
 pub enum Expr {
-    Unit {
-        variant: ExprVariant,
-        super_script: Option<Box<Expr>>,
-        sub_script: Option<Box<Expr>>,
-    },
-    Fraction {
-        numerator: Box<Expr>,
-        denominator: Box<Expr>,
-    },
+    Unit(Box<UnitExpr>),
+    Fraction(Box<Fraction>),
+}
+
+pub struct UnitExpr {
+    pub variant: ExprVariant,
+    pub super_script: Option<Expr>,
+    pub sub_script: Option<Expr>,
+}
+
+pub struct Fraction {
+    pub numerator: Expr,
+    pub denominator: Expr,
 }
 
 pub enum ExprVariant {
