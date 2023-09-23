86b22f99f87db7e27df966ed2bb0b0cf7d9d3518
Cabidge <cabbageboi69@gmail.com>
2023-09-23 18:56:32 -0400

    stub out Parser methods

--- src/math/parse/mod.rs
+++ src/math/parse/mod.rs
@@ -1,14 +1,31 @@
 pub mod expressions;
 pub mod tokenize;
 
+use std::iter::Peekable;
+
+use expressions::{Expr, GroupExpr, UnitExpr};
+use tokenize::token::GroupingKind;
+
 pub struct Parser<'a> {
-    token_stream: tokenize::Tokenizer<'a>,
+    token_stream: Peekable<tokenize::Tokenizer<'a>>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(input: &'a str) -> Self {
         Self {
-            token_stream: tokenize::Tokenizer::new(input),
+            token_stream: tokenize::Tokenizer::new(input).peekable(),
         }
+    }
+
+    fn parse_expr(&mut self) -> Option<Expr> {
+        todo!()
+    }
+
+    fn parse_unit(&mut self) -> Option<UnitExpr> {
+        todo!()
+    }
+
+    fn parse_grouping(&mut self, left: GroupingKind) -> GroupExpr {
+        todo!()
     }
 }
