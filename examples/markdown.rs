1aa450a8b530b082510a09b574f51e5d8b671db4
Cabidge <cabbageboi69@gmail.com>
2023-10-03 12:20:17 -0400

    add markdown example to make testing easier

---
+++ examples/markdown.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut md = markdown_it::MarkdownIt::new();
+
+    markdown_it::plugins::cmark::add(&mut md);
+
+    md.inline
+        .add_rule::<flashmark::markdown_it::math::InlineMathRule>();
+
+    let html = md.parse("Hello $`2 in { 1, 2, 3 }`$ world!").render();
+
+    println!("{}", html);
+}
