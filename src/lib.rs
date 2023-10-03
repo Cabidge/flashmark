2a35c36dd916ef4e7b2a718811497a7edce5512d
Cabidge <cabbageboi69@gmail.com>
2023-10-03 12:29:58 -0400

    add top level render function to apply all templating and rendering steps

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,16 @@ pub mod math;
 pub mod parsing;
 pub mod slides;
 pub mod template;
+
+pub fn render(input: &str) -> Vec<String> {
+    use markdown_it::MarkdownIt;
+
+    let mut md = MarkdownIt::new();
+
+    markdown_it::plugins::cmark::add(&mut md);
+    markdown::math::add(&mut md);
+
+    slides::Slides::new(&template::render(input))
+        .map(|slide| md.parse(slide).render())
+        .collect()
+}
