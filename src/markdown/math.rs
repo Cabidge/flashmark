8820e7f6554d09fa09a0eaa071a4fd4cb7a0be01
Cabidge <cabbageboi69@gmail.com>
2023-10-03 12:42:20 -0400

    modify markdown math plugin node to allow for block display

--- src/markdown/math.rs
+++ src/markdown/math.rs
@@ -1,13 +1,16 @@
 use markdown_it::{
     parser::inline::{InlineRule, InlineState},
-    Node, NodeValue, Renderer, MarkdownIt,
+    MarkdownIt, Node, NodeValue, Renderer,
 };
+
+use crate::math::DisplayMode;
 
 pub struct InlineMathRule;
 
 #[derive(Debug)]
-pub struct InlineMathNode {
-    pub rendered_body: String,
+pub struct MathNode {
+    pub display_mode: DisplayMode,
+    pub body: String,
 }
 
 pub fn add(md: &mut MarkdownIt) {
@@ -16,6 +19,20 @@ pub fn add(md: &mut MarkdownIt) {
 
 const LEFT_DELIM: &str = "$`";
 const RIGHT_DELIM: &str = "`$";
+
+impl MathNode {
+    pub fn new(display_mode: DisplayMode, input: &str) -> Self {
+        use crate::math::{self, parse};
+
+        let mut body = String::new();
+
+        let ast_parser = parse::Parser::new(input);
+
+        math::render_row(ast_parser, &mut body).expect("Writing to string should not fail");
+
+        Self { display_mode, body }
+    }
+}
 
 impl InlineRule for InlineMathRule {
     const MARKER: char = '$';
@@ -26,8 +43,7 @@ impl InlineRule for InlineMathRule {
         let input = input.strip_prefix(LEFT_DELIM)?;
         let length = input.find(RIGHT_DELIM)?;
 
-        let rendered_body = crate::math::render(&input[..length], crate::math::DisplayMode::Inline);
-        let node = Node::new(InlineMathNode { rendered_body });
+        let node = Node::new(MathNode::new(DisplayMode::Inline, &input[..length]));
 
         let full_length = LEFT_DELIM.len() + length + RIGHT_DELIM.len();
 
@@ -35,8 +51,15 @@ impl InlineRule for InlineMathRule {
     }
 }
 
-impl NodeValue for InlineMathNode {
+impl NodeValue for MathNode {
     fn render(&self, _node: &Node, fmt: &mut dyn Renderer) {
-        fmt.text_raw(&self.rendered_body);
+        let display = match self.display_mode {
+            DisplayMode::Inline => "inline",
+            DisplayMode::Block => "block",
+        };
+
+        fmt.open("math", &[("display", display.to_string())]);
+        fmt.text_raw(&self.body);
+        fmt.close("math");
     }
 }
