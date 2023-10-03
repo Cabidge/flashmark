93d2d7beb58644ad6092f29e9a53f6deb7c548dd
Cabidge <cabbageboi69@gmail.com>
2023-10-03 11:43:08 -0400

    add markdown_it inline math rule

--- src/markdown_it/math.rs
+++ src/markdown_it/math.rs
@@ -1,14 +1,38 @@
 use markdown_it::{
     parser::inline::{InlineRule, InlineState},
-    Node,
+    Node, NodeValue, Renderer,
 };
 
 pub struct InlineMathRule;
+
+#[derive(Debug)]
+pub struct InlineMathNode {
+    pub rendered_body: String,
+}
+
+const LEFT_DELIM: &str = "$`";
+const RIGHT_DELIM: &str = "`$";
 
 impl InlineRule for InlineMathRule {
     const MARKER: char = '$';
 
     fn run(state: &mut InlineState) -> Option<(Node, usize)> {
-        None
+        let input = &state.src[state.pos..state.pos_max];
+
+        let input = input.strip_prefix(LEFT_DELIM)?;
+        let length = input.find(RIGHT_DELIM)?;
+
+        let rendered_body = crate::math::render(&input[..length], crate::math::DisplayMode::Inline);
+        let node = Node::new(InlineMathNode { rendered_body });
+
+        let full_length = LEFT_DELIM.len() + length + RIGHT_DELIM.len();
+
+        Some((node, full_length))
+    }
+}
+
+impl NodeValue for InlineMathNode {
+    fn render(&self, _node: &Node, fmt: &mut dyn Renderer) {
+        fmt.text_raw(&self.rendered_body);
     }
 }
