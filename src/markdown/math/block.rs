cabadbbc44a1048347d4c6bb278d8bc3bb32cc17
Cabidge <cabbageboi69@gmail.com>
2023-10-05 12:37:24 -0400

    refactor with guard statements

--- src/markdown/math/block.rs
+++ src/markdown/math/block.rs
@@ -11,14 +11,18 @@ const LANGUAGE: &str = "math";
 impl CoreRule for MathFenceRule {
     fn run(root: &mut Node, _md: &MarkdownIt) {
         root.walk_mut(|node, _depth| {
-            if let Some(code_block) = node.cast_mut::<CodeFence>() {
-                if code_block.info.trim() == LANGUAGE {
-                    let mut math_node = Node::new(MathNode::new(&code_block.content));
-                    math_node.attrs.push(("display", "block".into()));
+            let Some(code_block) = node.cast_mut::<CodeFence>() else {
+                return;
+            };
 
-                    *node = math_node;
-                }
+            if code_block.info.trim() != LANGUAGE {
+                return;
             }
+
+            let mut math_node = Node::new(MathNode::new(&code_block.content));
+            math_node.attrs.push(("display", "block".into()));
+
+            *node = math_node;
         });
     }
 }
